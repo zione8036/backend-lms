eb59dd0616dabde4dd97977b341fa156744f15f2
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2022-09-27 08:39:19 +0800

    backend project creation

---
+++ api/src/test/java/biz/global/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.global;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
