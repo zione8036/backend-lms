c8a2e591c2c618eda0b6631c61e5ada011200d21
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2022-09-28 14:14:04 +0800

    ignorefile

--- api/src/main/java/biz/global/model/Student.java
+++ api/src/main/java/biz/global/model/Student.java
@@ -6,10 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-@Entity
-
-public class Student {
 import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.GenericGenerator;
