0e9a51362d9b076cd981ed8a5bd10708b24484a0
zione8036 <74102490+zione8036@users.noreply.github.com>
2022-09-28 15:15:50 +0800

    Initial Update

--- api/src/main/java/biz/global/repo/AuthRepo.java
+++ api/src/main/java/biz/global/repo/AuthRepo.java
@@ -2,8 +2,6 @@ package biz.global.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthRepo extends JpaRepository<Student, Long> {
-
 import biz.global.model.AdminModel;
 
 public interface AuthRepo extends JpaRepository<AdminModel, Long> {
