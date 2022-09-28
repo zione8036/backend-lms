51c2d2254270cc873ce161438c5d24c556871e95
zione8036 <74102490+zione8036@users.noreply.github.com>
2022-09-28 10:37:57 +0800

    Setting up of model - Student

--- api/src/main/java/biz/global/ApiApplication.java
+++ api/src/main/java/biz/global/ApiApplication.java
@@ -2,8 +2,11 @@ package biz.global;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"biz.global"})
+@EntityScan("biz.global.model")
 public class ApiApplication {
 
 	public static void main(String[] args) {
