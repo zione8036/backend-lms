263a31076a8f34acd1664f20f04017b83046e1df
nicamiranda <gennicamiranda@gmail.com>
2022-10-11 21:30:16 +0800

    done with professor CRUD
    done with professor CRUD

--- api/src/main/java/biz/global/controller/ProfessorController.java
+++ api/src/main/java/biz/global/controller/ProfessorController.java
@@ -1,19 +1,28 @@
 package biz.global.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import biz.global.exception.ResourceNotFoundException;
 import biz.global.model.Professor;
 import biz.global.model.ResponseModel;
+import biz.global.model.Student;
 import biz.global.model.Subject;
 import biz.global.repo.ProfessorRepo;
 
@@ -24,21 +33,54 @@ public class ProfessorController {
 	@Autowired
 	private ProfessorRepo professorRepo;
 	
-	
-	
 	@GetMapping(value= "all")
     List<Professor> getprofessors() {
         return professorRepo.findAll();
     }
 
     @PostMapping(value="add")
-    public ResponseEntity<ResponseModel> addProfessor(@RequestBody Professor professor) {
-    	Optional<Professor> prof = Optional.ofNullable(professorRepo.findByProfessorNo(professor.getProfessor_no()));
+    public ResponseEntity<ResponseModel> addProfessor(@RequestBody Professor professor) throws IOException {
+    	Optional<Professor> prof = Optional.ofNullable(professorRepo.findByProfessorNo(professor.getProfessorNo()));
     	if(prof.isPresent()) {
     		return ResponseEntity.ok().body(new ResponseModel(0, "professor code already exist", null, null));
     	}
+    	
+    	ObjectMapper mapper = new ObjectMapper();
+		System.out.print(mapper.writeValueAsString(professor));
+		
     	professorRepo.save(professor);
-        return ResponseEntity.ok().body(new ResponseModel(0, "professor code already exist", null, prof.get()));
+        return ResponseEntity.ok().body(new ResponseModel(0, "professor added successfully", null, professor));
     }
+    
+ 
+    @GetMapping("{id}")
+    public ResponseEntity<Professor> getEmployeeById(@PathVariable Long id){
+    	Professor professor = professorRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        return ResponseEntity.ok(professor);
+    }
+    
+    @PatchMapping(value="/update/{id}")
+	private ResponseEntity<ResponseModel> updateProfessor(@PathVariable Long id, @RequestBody Professor professor) {
+		Optional<Professor> prof = professorRepo.findById(id);
+		if(prof.isPresent()) {
+			professorRepo.save(professor);
+			return ResponseEntity.ok().body(new ResponseModel(1, "professor updated successfully", "", professor));
+		}
+		return ResponseEntity.ok().body(new ResponseModel(0, "An unexpected error occurred", "", null));
+	}
 
+    
+    @DeleteMapping("{id}")
+    public ResponseEntity<ResponseModel> deleteProfessor(@PathVariable Long id){
+    	Optional<Professor> professor = professorRepo.findById(id);
+    	if(professor.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseModel(0, "professor does not exist", null, null));
+		}
+    	
+		professorRepo.deleteById(professor.get().getProfessor_id());
+		
+		return ResponseEntity.ok().body(new ResponseModel(1, "successfully deleted", null, null));
+    }
+    
 }
--- api/src/main/java/biz/global/repo/ProfessorRepo.java
+++ api/src/main/java/biz/global/repo/ProfessorRepo.java
@@ -1,9 +1,11 @@
 package biz.global.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import biz.global.model.Professor;
 
+@Repository
 public interface ProfessorRepo extends JpaRepository<Professor, Long> {
-	Professor  findByProfessorNo(String professor_no);
+	Professor findByProfessorNo(String professorNo);
 }
