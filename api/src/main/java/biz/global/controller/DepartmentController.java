ed9e7d0838277bc1b6eff20fbcde27458d2c0db0
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2022-10-11 19:23:42 +0800

    student bugfix and department crud

--- api/src/main/java/biz/global/controller/DepartmentController.java
+++ api/src/main/java/biz/global/controller/DepartmentController.java
@@ -1,14 +1,16 @@
 package biz.global.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,6 +59,37 @@ public class DepartmentController {
 		List<Department> dep = departmentRepo.findAll();
 		List<Subject> subjects = subjectRepo.findAll();
 		return ResponseEntity.ok().body(new ResponseModel(1, "get all departments", "", dep, subjects));
+	}
+	
+	@GetMapping(value = "/findbyid/{id}")
+	private ResponseEntity<ResponseModel> getDepartmentByID(@PathVariable Long id) {
+		Optional<Department> department = departmentRepo.findById(id);
+		if(department.isPresent()) {
+			return ResponseEntity.ok().body(new ResponseModel(1, "department exist", "", department));
+		}
+		return ResponseEntity.ok().body(new ResponseModel(0, "department does not exist", "", department));
+	}
+	
+	@PostMapping(value = "/update/{id}")
+	private ResponseEntity<ResponseModel> updateDepartment(@PathVariable Long id, @RequestBody Department department) {
+		Optional<Department> dep = departmentRepo.findById(id);
+		if(dep.isPresent()) {
+			departmentRepo.save(department);
+			return ResponseEntity.ok().body(new ResponseModel(1, "department updated successfully", "", department));
+		}
+		return ResponseEntity.ok().body(new ResponseModel(0, "An unexpected error occurred", "", null));
+	}
+	
+	@DeleteMapping(value = "/delete/{id}")
+	private ResponseEntity<ResponseModel> deleteDepartment(@PathVariable Long id) {
+		Optional<Department> department = departmentRepo.findById(id);
+		if(department.isPresent()) {
+			
+			departmentRepo.deleteById(id);
+			return ResponseEntity.ok().body(new ResponseModel(1, "department successfully deleted", "", null));
+		}
+		
+		return ResponseEntity.ok().body(new ResponseModel(0, "An unexpected error occurred", "", null));
 	}
 
 }
